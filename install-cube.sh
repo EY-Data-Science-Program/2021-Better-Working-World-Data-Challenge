071655599745ea51c14494b69413a0bd483a6a28
Nick Butler <nick@codeindulgence.com>
2020-09-03 09:32:38 +1000

    Apt update before adding repo
    Attempt to see if this avoids signed-repository error

--- install-cube.sh
+++ install-cube.sh
@@ -18,7 +18,9 @@ set -ex
 # Install our dependencies
 export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
+apt-get update
 apt-key add docker.gpg 
+apt-key list
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update && apt-get install -y docker-ce python3-pip unzip wget
 pip3 install docker-compose
