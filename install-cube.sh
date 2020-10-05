4b51a3aa4aa9c347f97c3e8a6a1a5f11fbdec2d3
Alex Leith <alexgleith@gmail.com>
2020-10-05 12:57:22 +1100

    Update to add landsat and fix password bug

--- install-cube.sh
+++ install-cube.sh
@@ -11,9 +11,11 @@ EOS
 exit 1
 fi
 
-password="${1}"
+PASSWORD="${1}"
 
 set -ex
+# Log start time
+echo "Started $(date)"
 
 # Install our dependencies
 export DEBIAN_FRONTEND=noninteractive
@@ -31,14 +33,13 @@ unzip /tmp/archive.zip
 mv cube-in-a-box-dea-main /opt/odc
 
 # We need to change some local vars.
-sed --in-place 's/secretpassword/${SecretPassword}/g' /opt/odc/docker-compose.yml
+sed --in-place "s/secretpassword/${PASSWORD}/g" /opt/odc/docker-compose.yml
 
 # We need write access in these places
 chmod -R 777 /opt/odc/notebooks
 cd /opt/odc
 
 # Start the machines
-docker-compose pull
 docker-compose up -d
 
 # Wait for them to wake up
@@ -46,9 +47,20 @@ sleep 5
 
 # Initialise and load a product, and then some data
 # Note to future self, we can't use make here because of TTY interactivity (the -T flag)
+# Initialise the datacube DB
 docker-compose exec -T jupyter datacube -v system init
+# Add some custom metadata
 docker-compose exec -T jupyter datacube metadata add https://raw.githubusercontent.com/opendatacube/datacube-alchemist/local-dev-env/metadata.eo_plus.yaml
+docker-compose exec -T jupyter datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/eo3_landsat_ard.odc-type.yaml
+# And add some product definitions
 docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/ga_s2_ard_nbar/ga_s2_ard_nbar_granule.yaml
-docker-compose exec -T jupyter bash -c "gunzip -c < /scripts/vic-scenes.tar.gz | dc-index-from-tar"
+docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/products-aws/ard_ls8.odc-product.yaml
+docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/products-aws/ard_ls7.odc-product.yaml
+# docker-compose exec -T jupyter datacube product add /scripts/linescan.odc-product.yaml
+# Now index some datasets
+docker-compose exec -T jupyter bash -c "cat /scripts/s-2-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
+docker-compose exec -T jupyter bash -c "cat /scripts/ls7-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
+docker-compose exec -T jupyter bash -c "cat /scripts/ls8-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
+# docker-compose exec -T jupyter bash -c "s3-find s3://dea-public-data/projects/ey-challenge-2020/**/*.json | s3-to-tar --no-sign-request | dc-index-from-tar --stac"
 
-echo "Done, all done. It should work now!"
+echo "Finished $(date)"
