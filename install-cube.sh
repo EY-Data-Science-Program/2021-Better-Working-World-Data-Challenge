53a92ac6c8de4bb56a0a22d8a0f7094274102960
David Gavin <GypsyBojangles@users.noreply.github.com>
2020-11-20 12:06:00 +1100

    Add Azure indexing to install script (#11)
    * Add Azure indexing to install script
    
    Add Azure indexing to install script
    This is untested so please test befpre merging - docker is not my strong suite.
    
    * Tested local Docker environment
    
    Co-authored-by: Alex Leith <alexgleith@gmail.com>

--- install-cube.sh
+++ install-cube.sh
@@ -11,56 +11,62 @@ EOS
 exit 1
 fi
 
+# Capture a password
 PASSWORD="${1}"
+
+# Do it different if it's local Docker
+LOCAL="${2}"
 
 set -ex
 # Log start time
 echo "Started $(date)"
 
 # Install our dependencies
-export DEBIAN_FRONTEND=noninteractive
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
-apt-get update
-apt-key add docker.gpg 
-apt-key list
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update && apt-get install -y docker-ce python3-pip unzip wget
-pip3 install docker-compose
+if ! [[ $LOCAL = "true" ]]; then
+  export DEBIAN_FRONTEND=noninteractive
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg > docker.gpg
+  apt-get update
+  apt-key add docker.gpg 
+  apt-key list
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  apt-get update && apt-get install -y docker-ce python3-pip unzip wget
+  pip3 install docker-compose
 
-# Get our code
-wget https://github.com/EY-Data-Science-Program/2020-bushfire-challenge/archive/main.zip -O /tmp/archive.zip
-unzip /tmp/archive.zip
-mv 2020-bushfire-challenge-main /opt/odc
+  # Get our code
+  wget https://github.com/EY-Data-Science-Program/2020-bushfire-challenge/archive/main.zip -O /tmp/archive.zip
+  unzip /tmp/archive.zip
+  mv 2020-bushfire-challenge-main /opt/odc
 
-# We need to change some local vars.
-sed --in-place "s/secretpassword/${PASSWORD}/g" /opt/odc/docker-compose.yml
+  # We need to change some local vars.
+  sed --in-place "s/secretpassword/${PASSWORD}/g" /opt/odc/docker-compose.yml
 
-# We need write access in these places
-chmod -R 777 /opt/odc/notebooks
-cd /opt/odc
+  # We need write access in these places
+  chmod -R 777 /opt/odc/notebooks
+  cd /opt/odc
 
-# Start the machines
-docker-compose up -d
+  # Start the machines
+  docker-compose up -d
 
-# Wait for them to wake up
-sleep 5
+  # Wait for them to wake up
+  sleep 5
+fi
 
 # Initialise and load a product, and then some data
 # Note to future self, we can't use make here because of TTY interactivity (the -T flag)
 # Initialise the datacube DB
 docker-compose exec -T jupyter datacube -v system init
 # Add some custom metadata
-docker-compose exec -T jupyter datacube metadata add /scripts/metadata.eo_plus.yaml
-docker-compose exec -T jupyter datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/eo3_landsat_ard.odc-type.yaml
+docker-compose exec -T jupyter datacube metadata add /scripts/data/metadata.eo_plus.yaml
+docker-compose exec -T jupyter datacube metadata add /scripts/data/eo3_landsat_ard.odc-type.yaml
 # And add some product definitions
-docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/ga_s2_ard_nbar/ga_s2_ard_nbar_granule.yaml
-docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/products-aws/ard_ls8.odc-product.yaml
-docker-compose exec -T jupyter datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/eo3/products-aws/ard_ls7.odc-product.yaml
-docker-compose exec -T jupyter datacube product add /scripts/linescan.odc-product.yaml
+docker-compose exec -T jupyter datacube product add /scripts/data/ga_s2a_ard_nbar_granule.odc-product.yaml
+docker-compose exec -T jupyter datacube product add /scripts/data/ga_s2b_ard_nbar_granule.odc-product.yaml
+docker-compose exec -T jupyter datacube product add /scripts/data/ga_ls7e_ard_3.odc-product.yaml
+docker-compose exec -T jupyter datacube product add /scripts/data/ga_ls8c_ard_3.odc-product.yaml
+docker-compose exec -T jupyter datacube product add /scripts/data/linescan.odc-product.yaml
 # Now index some datasets
-docker-compose exec -T jupyter bash -c "cat /scripts/s-2-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
-docker-compose exec -T jupyter bash -c "cat /scripts/ls7-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
-docker-compose exec -T jupyter bash -c "cat /scripts/ls8-vic-scenes.txt | s3-to-tar --no-sign-request | dc-index-from-tar --ignore-lineage"
-docker-compose exec -T jupyter bash -c "s3-find --no-sign-request s3://dea-public-data/projects/ey-2020-bushfire-challenge/**/*.odc-dataset.json | s3-to-tar --no-sign-request | dc-index-from-tar"
+docker-compose exec -T jupyter bash -c "dc-index-from-tar --protocol https --ignore-lineage -p ga_ls7e_ard_3 -p ga_ls8c_ard_3 /scripts/data/ls78.tar.gz"
+docker-compose exec -T jupyter bash -c "dc-index-from-tar --protocol https --ignore-lineage -p ga_s2a_ard_nbar_granule -p ga_s2b_ard_nbar_granule /scripts/data/s2ab.tar.gz"
+docker-compose exec -T jupyter bash -c "dc-index-from-tar --protocol https --ignore-lineage -p linescan /scripts/data/linescan.tar.gz"
 
 echo "Finished $(date)"
