a99bb2d8dcd74fa65ba4c56d351348ab987cd8c7
Nick Butler <nick@codeindulgence.com>
2021-02-17 16:55:29 +1100

    Upgrade pip before docker-compose install

--- install-cube.sh
+++ install-cube.sh
@@ -30,6 +30,7 @@ if ! [[ $LOCAL = "true" ]]; then
   apt-key list
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   apt-get update && apt-get install -y docker-ce python3-pip unzip wget
+  pip3 install --upgrade pip
   pip3 install docker-compose
 
   # Get our code
