04421b206bc81fdcc660537f80058a74f2d8a827
David Gavin <GypsyBojangles@users.noreply.github.com>
2021-03-19 16:26:33 +1100

    Add Sentinel-1 Indexing

--- scripts/data/add_azure_data.sh
+++ scripts/data/add_azure_data.sh
@@ -7,7 +7,9 @@ datacube product add ga_ls8c_ard_3.odc-product.yaml
 datacube product add ga_s2a_ard_nbar_granule.odc-product.yaml
 datacube product add ga_s2b_ard_nbar_granule.odc-product.yaml
 datacube product add linescan.odc-product.yaml
+datacube product add esa_s1_rtc.odc-product.yaml
 
 dc-index-from-tar --protocol https --ignore-lineage -p "ga_ls7e_ard_3" -p "ga_ls8c_ard_3" ls78.tar.gz
 dc-index-from-tar --protocol https --ignore-lineage -p "ga_s2a_ard_nbar_granule" -p "ga_s2b_ard_nbar_granule" s2ab.tar.gz
 dc-index-from-tar --protocol https --ignore-lineage -p "linescan" linescan.tar.gz
+dc-index-from-tar --stac --protocol https --ignore-lineage --no-auto-add-lineage --no-verify-lineage -p "s1_rtc" sentinel1.tar.gz
