63b3f695bdacd4dc9831b2c4608bd539e95ab2ab
David Gavin <GypsyBojangles@users.noreply.github.com>
2021-03-20 10:24:20 +1100

    Update scripts/data/add_azure_data.sh
    Co-authored-by: Alex Leith <alexgleith@gmail.com>

--- scripts/data/add_azure_data.sh
+++ scripts/data/add_azure_data.sh
@@ -12,4 +12,4 @@ datacube product add esa_s1_rtc.odc-product.yaml
 dc-index-from-tar --protocol https --ignore-lineage -p "ga_ls7e_ard_3" -p "ga_ls8c_ard_3" ls78.tar.gz
 dc-index-from-tar --protocol https --ignore-lineage -p "ga_s2a_ard_nbar_granule" -p "ga_s2b_ard_nbar_granule" s2ab.tar.gz
 dc-index-from-tar --protocol https --ignore-lineage -p "linescan" linescan.tar.gz
-dc-index-from-tar --stac --protocol https --ignore-lineage --no-auto-add-lineage --no-verify-lineage -p "s1_rtc" sentinel1.tar.gz
+dc-index-from-tar --protocol https --ignore-lineage --stac -p "s1_rtc" sentinel1.tar.gz
